637675d06f4efe43d7c4435dd174c2474000cac4
uqlib <uqlibcom@gmail.com>
2021-11-30 16:14:37 +0900

    Remove unnecessary dll import statement

--- Challenge/DLL/src/UqLib.h
+++ Challenge/DLL/src/UqLib.h
@@ -10,13 +10,6 @@
 #include "Logger.h"
 #include "Constants.h"
 
-// DirectX11系の静的ライブラリをリンク
-#pragma comment(lib, "d2d1.lib")
-#pragma comment(lib, "dwrite.lib")
-#pragma comment(lib, "windowscodecs.lib")//テキスト表示用
-#pragma comment(lib, "dxgi.lib")
-#pragma comment(lib, "d3d11.lib")
-
 extern "C" __declspec (dllexport) int UqLibInit(const char* title);
 
 extern "C" __declspec (dllexport) int UqLibEnd();
