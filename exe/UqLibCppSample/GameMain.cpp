3210795465ccbf55dda2a47ce88b166e83707169
uqlib <uqlibcom@gmail.com>
2021-12-03 08:53:09 +0900

    Delete unnecessary files

--- exe/UqLibCppSample/GameMain.cpp
+++ exe/UqLibCppSample/GameMain.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-typedef int(*INIT_SCREEN_SIZE)(int width, int height);
 typedef int(*UQ_LIB_INIT)(const char* title);
-typedef int(*UPDATE_WINDOW_MESSAGE)();
+typedef int(*UQ_LIB_END)();
 typedef int(*BEGIN_DRAWING)();
 typedef int(*END_DRAWING)();
-typedef int(*UQ_LIB_END)();
-typedef int(*CREATE_TEXTURE_FROM_FILE)(const char* filename);
+typedef int(*INIT_SCREEN_SIZE)(int width, int height);
+typedef int(*CREATE_TEXTURE_FROM_FILE)(const char* fileName);
 typedef int(*DRAW_TEXTURE)(int x, int y, int graphicId);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -18,12 +17,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	if (NULL == hDllInstance) {
 		return 1;
 	}
-	INIT_SCREEN_SIZE InitScreenSize = (INIT_SCREEN_SIZE)GetProcAddress(hDllInstance, "InitScreenSize");
 	UQ_LIB_INIT UqLibInit = (UQ_LIB_INIT)GetProcAddress(hDllInstance, "UqLibInit");
-	UPDATE_WINDOW_MESSAGE UpdateWindowMessage = (UPDATE_WINDOW_MESSAGE)GetProcAddress(hDllInstance, "UpdateWindowMessage");
+	UQ_LIB_END UqLibEnd = (UQ_LIB_END)GetProcAddress(hDllInstance, "UqLibEnd");
 	BEGIN_DRAWING BeginDrawing = (BEGIN_DRAWING)GetProcAddress(hDllInstance, "BeginDrawing");
 	END_DRAWING EndDrawing = (END_DRAWING)GetProcAddress(hDllInstance, "EndDrawing");
-	UQ_LIB_END UqLibEnd = (UQ_LIB_END)GetProcAddress(hDllInstance, "UqLibEnd");
+	INIT_SCREEN_SIZE InitScreenSize = (INIT_SCREEN_SIZE)GetProcAddress(hDllInstance, "InitScreenSize");
 	CREATE_TEXTURE_FROM_FILE CreateTextureFromFile = (CREATE_TEXTURE_FROM_FILE)GetProcAddress(hDllInstance, "CreateTextureFromFile");
 	DRAW_TEXTURE DrawTexture = (DRAW_TEXTURE)GetProcAddress(hDllInstance, "DrawTexture");
 
@@ -36,7 +34,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int bmp1 = 0;
 	bmp1 = CreateTextureFromFile("test.png");
 
-	while (UpdateWindowMessage() == 0 && BeginDrawing() == 0) {
+	while (BeginDrawing() == 0) {
 		DrawTexture(0, 0, bmp1);
 		EndDrawing();
 	}
